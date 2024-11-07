67420e839e613c8b22bb291cc297c4a324ca3ee3
jaydenelliott <jayden.elliott@outlook.com>
2024-11-08 08:17:09 +1100

    update

--- term/nvim/lua/autocommands.lua
+++ term/nvim/lua/autocommands.lua
@@ -12,3 +12,13 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = '*/.ssh/config.d/*',
   command = 'set filetype=sshconfig'
 })
+
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+  local default_diagnostic_handler = vim.lsp.handlers[method]
+  vim.lsp.handlers[method] = function(err, result, context, config)
+    if err ~= nil and err.code == -32802 then
+      return
+    end
+    return default_diagnostic_handler(err, result, context, config)
+  end
+end
