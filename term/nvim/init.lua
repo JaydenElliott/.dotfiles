dfe1676de2650d8eb84478cd85dc21e35092d7fb
jaydenelliott <jayden.elliott@outlook.com>
2026-03-28 09:36:31 +1100

    update

--- term/nvim/init.lua
+++ term/nvim/init.lua
@@ -338,8 +338,7 @@ require("lazy").setup(
           "rust_analyzer",
           "lua_ls",
           "clangd",
-          "jsonls",
-          "pyright"
+          "jsonls"
         },
 
         servers = {
@@ -457,6 +456,15 @@ require("lazy").setup(
     },
   }
 )
+
+-- Open telescope on startup if no file given
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argv(0) == "" then
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
 
 
 -- custom commands
